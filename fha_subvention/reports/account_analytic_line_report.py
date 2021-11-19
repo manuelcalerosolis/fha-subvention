ed38a152c9cda8f4cd5041cf38ecc8fec4ee9089
Manuel Calero <manuelcalerosolis@gmail.com>
2021-11-19 10:56:06 +0100

    report grouped by employee and partner

--- fha_subvention/reports/account_analytic_line_report.py
+++ fha_subvention/reports/account_analytic_line_report.py
@@ -7,7 +7,7 @@ from odoo import api, fields, models
 class AccountAnalyticLineView(models.TransientModel):
     _name = "account.analytic.line.view"
     _description = "Subvention View"
-    _order = "date"
+    _order = "account_id, employee_id, move_id.partner_id, date"
 
     date = fields.Datetime()
     name = fields.Char()
@@ -19,7 +19,12 @@ class AccountAnalyticLineView(models.TransientModel):
     move_id = fields.Many2one(
         comodel_name='account.move.line',
     )
-
+    employee_id = fields.Many2one(
+        comodel_name='hr.employee',
+    )
+    partner_id = fields.Many2one(
+        comodel_name='res.partner'
+    )
 
 class AccountAnalyticLineReport(models.TransientModel):
     _name = "account.analytic.line.report"
@@ -36,6 +41,12 @@ class AccountAnalyticLineReport(models.TransientModel):
     currency_id = fields.Many2one(
         comodel_name='res.currency',
         default=lambda self: self.env.company.currency_id.id,
+    )
+    partner_id = fields.Many2one(
+        comodel_name="res.partner",
+    )
+    employee_id = fields.Many2one(
+        comodel_name="hr.employee",
     )
     # Data fields, used to browse report data
     results = fields.Many2many(
@@ -66,16 +77,21 @@ class AccountAnalyticLineReport(models.TransientModel):
                 ABS(line.amount) AS amount,
                 line.justified_amount,
                 line.account_id,
-                line.move_id
+                line.employee_id,
+                line.move_id,
+                partner.id AS partner_id
             FROM
                 account_analytic_line line
+                LEFT JOIN account_move_line account_move_line ON account_move_line.id = line.move_id
+                LEFT JOIN account_move account_move ON account_move.id = account_move_line.move_id
+                LEFT JOIN res_partner partner ON partner.id = account_move.partner_id
             WHERE
                 CAST(line.date AS date) >= %s
                 and
                 CAST(line.date AS date) <= %s
                 and
-                account_id in %s
-            ORDER BY line.date
+                line.account_id in %s
+            ORDER BY line.account_id, line.employee_id, partner_id, line.date
         """,
             (
                 date_from,
@@ -86,7 +102,8 @@ class AccountAnalyticLineReport(models.TransientModel):
         results = self._cr.dictfetchall()
 
         print(":"*80)
-        print("Results :", results)
+        for result in results:
+            print("Result :", result)
         print(":"*80)
 
         self.results = [ReportLine.new(line).id for line in results]
