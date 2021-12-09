913b400ad52974e93b38325f52918fa83b639040
Manuel Calero <manuelcalerosolis@gmail.com>
2021-12-09 13:12:10 +0100

    new module employee import

---
+++ fha_employee_import/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "FHA Employee Import",
+    "category": "Product",
+    "version": "13.0.1.0",
+    "depends": [
+        "hr_timesheet"
+    ],
+    "description": """
+        Wizard to Import FHA Employee.
+        """,
+    "data": [
+        "wizard/infortisa_product_import.xml",
+        "views/menu.xml",],
+    "installable": True,
+    "auto_install": True,
+}
