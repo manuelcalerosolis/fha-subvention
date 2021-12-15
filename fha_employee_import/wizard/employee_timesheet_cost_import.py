00a7c55dcc9ad403ce576464ccbb7bb9f40a5f51
Manuel Calero <manuelcalerosolis@gmail.com>
2021-12-15 13:51:44 +0100

    new module employee import finish

--- fha_employee_import/wizard/employee_timesheet_cost_import.py
+++ fha_employee_import/wizard/employee_timesheet_cost_import.py
@@ -1,3 +1,6 @@
+# Copyright 2021 Xtendoo Corporation
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
 import logging
 from base64 import b64decode
 from io import StringIO
@@ -33,20 +36,38 @@ class EmployeeTimesheetCostImport(models.TransientModel):
             return
         self._parse_file(data_file)
 
+    def update_employee(self, name, cost):
+        if name == "":
+            return _("Employee is empty")
+
+        if cost == "":
+            return _("Cost is empty in employee %s") % name
+
+        cost = float(cost.replace(",", "."))
+        if cost <= 0:
+            return _("Time sheet must be positive")
+
+        employee = self.env['hr.employee'].search([('name', 'ilike', name)], limit=1)
+        if not employee:
+            return _("Employee %s not found") % name
+
+        employee.sudo().write(
+                {
+                    "timesheet_cost": cost,
+                }
+            )
+        return False
+
     def parse_row(self, csv_data):
         error_list = []
         for row in csv_data:
-            print("*"*80)
-            print("row", row[1])
-            print("*"*80)
+            error = self.update_employee(row[0], row[1])
+            if error:
+                error_list.append(error)
 
-            if row[1] == "":
-                error_list.append(
-                    _("Employee is empty: %s") % (row[1])
-                )
         if error_list:
-            raise ValidationError(
-                _("Has errors: " "\n%s") % "\n".join(error_list)
+            raise UserError(
+                _("Has errors, correct the errors and retry: " "\n%s") % "\n".join(error_list)
             )
 
     def _parse_file(self, data_file):
