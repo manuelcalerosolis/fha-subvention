b927723df426a01d4d85a40be143511b35d0a449
manuelcalerosolis <manuelcalerosolis@gmail.com>
2022-05-05 09:12:38 +0200

    purchase_departments

---
+++ purchase_subventions/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2022 Xtendoo
+
+{
+    "name": "Purchase subventions",
+    "summary": """
+        Purchase-subventions""",
+    "version": "13.0",
+    "depends": ["purchase","fha_subvention"],
+    "maintainers": ["dariocruzmauro"],
+    "author": "Xtendoo",
+    "license": "AGPL-3",
+    "data": [
+        "views/purchase_order_views.xml"
+            ],
+    "installable": True,
+    "auto_install": True,
+}
+
